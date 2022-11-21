064186a5ffd0f497ca213d4b26c9d205ef3a058f
hardethanolninja <79213831+hardethanolninja@users.noreply.github.com>
2022-11-20 19:41:33 -0600

    big update to map functions, can process most post

--- scratch/script.js
+++ scratch/script.js
@@ -1,76 +1,106 @@
-const arr = [
-  "23a",
-  "23b",
-  "24a",
-  "24b",
-  "24c",
-  "24d",
-  "24e",
-  "25b",
-  "25e",
-  "25i",
-  "25j",
-  "25k",
-  "26a",
-  "26b",
-  "26c",
-  "26d",
-  "27h",
-  "27i",
-  "27j",
-  "29b",
-  "29c",
-  "29d",
-  "29e",
-  "29f",
-  "30a",
-  "30b",
-  "30c",
-  "30d",
-  "31a",
-  "31b",
-  "31c",
-  "31d",
-  "32a",
-  "32b",
-  "32c",
-  "33a",
-  "33b",
-  "33c",
-  "33d",
-  "33e",
-  "33f",
-  "34a",
-  "34b",
-  "34c",
-  "34d",
-  "34e",
-  "34f",
-  "34g",
-  "34h",
-  "34i",
-  "35a",
-  "35b",
-  "35c",
-  "35e",
-  "35f",
-  "35g",
-];
+const url = "https://npsot.us/wp-json/wp/v2/map-item/?per_page=100";
 
-console.log(arr.length);
+const mapArr = [];
 
-const ecoNames = [];
+// https://github.com/masajid390/BeautifyMarker
+const iconOptions = {
+  //   icon: "shop",
+  borderColor: "#3c5799",
+  textColor: "#3c5799",
+  //   backgroundColor: NICENurseries[ind].Shipping
+  //     ? "rgba(136, 155, 206,0.8)"
+  //     : "rgba(255,255,255,0.8)",
+  iconShape: "marker",
+  iconSize: [30, 30],
+  iconAnchor: [15, 30],
+  innerIconAnchor: [0, 7],
+};
 
-for (ele of arr) {
-  const getData = async () => {
-    const queryURL = `https://geodata.epa.gov/arcgis/rest/services/ORD/USEPA_Ecoregions_Level_III_and_IV/MapServer/7/query?where=US_L4CODE+%3D+%27${ele}%27&f=pjson`;
-    try {
-      const res = await fetch(queryURL);
-      const json = await res.json();
-      ecoNames.push(json.features[0].attributes);
-    } catch (error) {
-      console.log(error);
+const fetchStuff = async (params) => {
+  let map = L.map("ecoregion_map", { zoomSnap: 0.25 }).setView(
+    [31.75, -99.9],
+    5.75
+  );
+
+  L.tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
+    {
+      maxZoom: 10,
+      minZoom: 5,
+      attribution:
+        "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community",
     }
-  };
-  getData();
-}
+  ).addTo(map);
+
+  //add layer controller to map.  this is used for the layer toggle checkboxes
+  const layerControl = L.control
+    .layers(null, null, { collapsed: false })
+    .addTo(map);
+
+  try {
+    let mapRes = await fetch(url);
+    let mapJSON = await mapRes.json();
+    mapArr.push(mapJSON);
+  } catch (error) {
+    console.log(error);
+  }
+  const mapData = await Promise.all(mapArr);
+
+  if (params.nice) {
+    //initialize layer for NICE pins
+    const NICEoms = new OverlappingMarkerSpiderfier(map);
+    const NICEPins = new L.LayerGroup();
+
+    const NICEOptions = iconOptions;
+    NICEOptions.icon = "shop";
+
+    const niceData = mapData[0].filter(
+      (ele) => ele.acf.map_item_type === "NICE Partner"
+    );
+
+    for (nursery of niceData) {
+      //pick color based on shipping
+      nursery.acf.map_item_shipping === "Yes"
+        ? (NICEOptions.backgroundColor = "rgba(136, 155, 206,0.8)")
+        : (NICEOptions.backgroundColor = "rgba(255,255,255,0.8)");
+
+      //build tooltip
+      let tooltip = `<h3 class=map-item__title>${nursery.title.rendered}</h3>`;
+
+      //build popup
+      let popup = `<h3 class=map-item__title>${nursery.title.rendered}</h3><br>
+      ${
+        nursery.acf.map_item_shipping === "Yes"
+          ? "<p class='map-item__text'><b>We Ship!</b></p><br>"
+          : ""
+      }
+      <a class='map-item__link' target='_blank' href=${
+        nursery.acf.map_item_external_url
+      }>Visit our Homepage</a><br>`;
+
+      if (nursery.acf.map_item_associated_organization) {
+        popup += `<p class='nice-text'><b>Associated Chapters</b></p>`;
+        nursery.acf.map_item_associated_organization.forEach((ele) => {
+          const chapter = ele.replace(/ +/g, "-").toLowerCase();
+          popup += `<a class='map-item__link' href='https://npsot.us/chapter/${chapter}' target='_blank'>${ele}</a><br>`;
+        });
+      }
+
+      const marker = L.marker(
+        [nursery.acf.map_item_loc.lat, nursery.acf.map_item_loc.lng],
+        {
+          icon: L.BeautifyIcon.icon(NICEOptions),
+        }
+      )
+        .addTo(NICEPins)
+        .bindTooltip(tooltip)
+        .bindPopup(popup);
+
+      NICEoms.addMarker(marker);
+    }
+
+    map.addLayer(NICEPins);
+    layerControl.addOverlay(NICEPins, "NICE! Nurseries");
+  }
+};
+fetchStuff({ nice: true });
